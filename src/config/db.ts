a8fb868803911d6b27e35ebd7088a156a0024553
Anandu R Nair <anandurpallam@gmail.com>
2024-09-09 00:22:13 +0530

    mongo error solved

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,22 @@ import mongoose from 'mongoose';
 
 function dbConnection() {
     const URL: string = process.env.MONGO_URL as string;
-    mongoose.connect(URL)
+
+    mongoose.connect(URL, {
+        serverSelectionTimeoutMS: 30000
+    })
     .then(() => console.log('Connection successful'))
-    .catch((err: Error) => console.log(err));
+    .catch((err: Error) => console.error('Database connection error:', err));
+
+    // Handle connection events
+    mongoose.connection.on('error', (err) => {
+        console.error('Database error:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.warn('Database disconnected');
+    });
 }
 
 export default dbConnection;
+
